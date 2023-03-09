243bdacc71518bd8fe2a076a69963144f898b436
SeanAth017 <sean@atho.com.au>
2023-03-10 10:52:29 +1100

    Program Restructure

--- src/rentalservice/Customer.java
+++
@@ -1,9 +0,0 @@
-package rentalservice;
-
-public class Customer {
-    // Methods:
-    //
-    // Constructor: to initialize the attributes of the customer.
-    // Getter and Setter methods for each attribute.
-    // validateEmailAddress(String email): to validate the email address of the customer.
-}
--- src/rentalservice/Main.java
+++ src/rentalservice/Main.java
@@ -1,16 +1,4 @@
 package rentalservice;
 
 public class Main {
-    // Methods:
-    //
-    // showMainMenu(): to show the main menu.
-    // showVehicleTypes(): to show all available vehicle types.
-    // showVehiclesByType(String type): to show all vehicles under a specific type.
-    // showBrands(): to show all available brands.
-    // showVehiclesByBrand(String brand): to show all vehicles under a specific brand.
-    // showFilteredVehicles(int numPassengers): to show all vehicles that can carry at least a certain number of passengers.
-    // reserveVehicle(Vehicle vehicle): to reserve a vehicle for a customer.
-    // enterPersonalInfo(): to enter personal information of the customer.
-    // confirmAndPay(Reservation reservation, Customer customer): to confirm and pay for the reservation.
-    // showReservationDetails(Reservation reservation, Customer customer): to show the reservation details after confirmation and payment.
 }
