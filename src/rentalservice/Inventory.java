681d0b7c78f817b66bf6d8052684a1f51f2a597f
SeanAth017 <sean@atho.com.au>
2023-03-10 14:36:48 +1100

    Additonal Changes to Inventory Class

--- src/rentalservice/Inventory.java
+++ src/rentalservice/Inventory.java
@@ -1,10 +1,8 @@
 package rentalservice;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class Inventory {
-    private HashMap<String, Integer> availableCars;
     private ArrayList<Car> cars;
     private double rentalPrice;
     private double insurancePrice;
@@ -13,22 +11,14 @@ public class Inventory {
 
     public Inventory(ArrayList<Car> cars, double rentalPrice, double insurancePrice, double serviceFee, double discount) {
         this.cars = cars;
-        this.availableCars = new HashMap<>();
-        for (Car car : cars) {
-            this.availableCars.put(car.getVehicleID(), 1);
-        }
         this.rentalPrice = rentalPrice;
         this.insurancePrice = insurancePrice;
         this.serviceFee = serviceFee;
-        this.discount = discount/100.0; // Convert discount percentage to decimal value
+        this.discount = discount;
     }
 
     public ArrayList<Car> getCars() {
         return cars;
-    }
-
-    public HashMap<String, Integer> getAvailableCars() {
-        return availableCars;
     }
 
     public double getRentalPrice() {
@@ -56,29 +46,11 @@ public class Inventory {
     }
 
     public double getDiscount() {
-        return discount * 100; // Convert discount decimal value to percentage
+        return discount;
     }
 
     public void setDiscount(double discount) {
-        this.discount = discount/100.0; // Convert discount percentage to decimal value
-    }
-
-    public boolean isCarAvailable(Car car) {
-        return this.availableCars.get(car.getVehicleID()) != null && this.availableCars.get(car.getVehicleID()) > 0;
-    }
-
-    public void rentCar(Car car) {
-        if (isCarAvailable(car)) {
-            int available = this.availableCars.get(car.getVehicleID());
-            this.availableCars.put(car.getVehicleID(), available - 1);
-        } else {
-            throw new IllegalArgumentException("Car is not available for rent.");
-        }
-    }
-
-    public void returnCar(Car car) {
-        int available = this.availableCars.get(car.getVehicleID());
-        this.availableCars.put(car.getVehicleID(), available + 1);
+        this.discount = discount;
     }
 
     public double calculateRentalFee(int numOfDays) {
