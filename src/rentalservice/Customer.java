d42766112c1c0e8403f18cca89cf18b3c64f3df8
SeanAth017 <sean@atho.com.au>
2023-03-11 13:43:20 +1100

    Customer Class and Comments

--- src/rentalservice/Customer.java
+++ src/rentalservice/Customer.java
@@ -1,5 +1,38 @@
 package rentalservice;
 // Represents a customer object with personal details such as name, email, and number of passengers.
 public class Customer {
-    
+    // Instance variables
+    private String name;
+    private String email;
+    private int numPassengers;
+
+    // Constructor
+    public Customer(String name, String email, int numPassengers) {
+        this.name = name;
+        this.email = email;
+        this.numPassengers = numPassengers;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getNumPassengers() {
+        return numPassengers;
+    }
+
+    // toString method to display customer information
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", numPassengers=" + numPassengers +
+                '}';
+    }
 }
