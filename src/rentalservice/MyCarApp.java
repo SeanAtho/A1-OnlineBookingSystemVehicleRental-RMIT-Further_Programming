613f1c39f399779ffc78677040f4a10f9d11420e
SeanAth017 <sean@atho.com.au>
2023-03-14 09:06:52 +1100

    Method Changes to MyCarApp

--- src/rentalservice/MyCarApp.java
+++ src/rentalservice/MyCarApp.java
@@ -1,134 +1,76 @@
 package rentalservice;
-// Acts as the controller, orchestrating interactions between the model (Inventory, Rental, Customer)
-// and view (ConsoleUI) classes, and implementing program logic such as searching, filtering, and rental booking.
 import java.time.LocalDate;
-import java.util.List;
-import java.util.Scanner;
 
 public class MyCarApp {
     private Inventory inventory;
-    private Scanner scanner;
 
     public MyCarApp(Inventory inventory) {
         this.inventory = inventory;
-        this.scanner = new Scanner(System.in);
     }
 
     public void start() {
         System.out.println("Welcome to MyCarApp!");
-        boolean running = true;
-        while (running) {
-            System.out.println("Please select an option:");
-            System.out.println("1. Search for cars by brand");
-            System.out.println("2. Browse cars by type");
-            System.out.println("3. Filter cars by number of seats");
-            System.out.println("4. View car details");
-            System.out.println("5. View available cars");
-            System.out.println("6. Rent a car");
-            System.out.println("7. Exit");
-
-            int choice = scanner.nextInt();
-            scanner.nextLine(); // Consume newline character
-
-            switch (choice) {
-                case 1:
-                    System.out.println("Please enter a brand name:");
-                    String brand = scanner.nextLine();
-                    searchByBrand(brand);
-                    break;
-                case 2:
-                    System.out.println("Please enter a car type:");
-                    String type = scanner.nextLine();
-                    browseByType(type);
-                    break;
-                case 3:
-                    System.out.println("Please enter the number of seats:");
-                    int numSeats = scanner.nextInt();
-                    filterBySeats(numSeats);
-                    break;
-                case 4:
-                    System.out.println("Please enter a car ID:");
-                    int carId = scanner.nextInt();
-                    scanner.nextLine(); // Consume newline character
-                    viewCarDetails(carId);
-                    break;
-                case 5:
-                    viewAvailableCars();
-                    break;
-                case 6:
-                    System.out.println("Please enter the car ID:");
-                    carId = scanner.nextInt();
-                    scanner.nextLine(); // Consume newline character
-                    System.out.println("Please enter your name:");
-                    String name = scanner.nextLine();
-                    System.out.println("Please enter your email address:");
-                    String email = scanner.nextLine();
-                    System.out.println("Please enter the number of passengers:");
-                    int numPassengers = scanner.nextInt();
-                    System.out.println("Please enter the start date (yyyy-mm-dd):");
-                    LocalDate startDate = LocalDate.parse(scanner.next());
-                    System.out.println("Please enter the end date (yyyy-mm-dd):");
-                    LocalDate endDate = LocalDate.parse(scanner.next());
-                    bookRental(carId, new Customer(name, email, numPassengers), startDate, endDate);
-                    break;
-                case 7:
-                    running = false;
-                    break;
-                default:
-                    System.out.println("Invalid choice, please try again.");
-            }
-        }
-        System.out.println("Thanks for using MyCarApp!");
     }
 
     public void searchByBrand(String brand) {
-        List<Car> cars = inventory.searchByBrand(brand);
-        if (cars.isEmpty()) {
-            System.out.println("No cars found.");
-        } else {
-            System.out.println("Cars matching " + brand + ":");
-            for (Car car : cars) {
-                System.out.println(car.toString());
+        System.out.println("Searching for cars by brand: " + brand);
+        for (Car car : inventory.getCars()) {
+            if (car.getBrand().equalsIgnoreCase(brand)) {
+                System.out.println(car);
             }
         }
     }
 
     public void browseByType(String type) {
-        List<Car> cars = inventory.filterByType(type);
-        if (cars.isEmpty()) {
-            System.out.println("No cars found.");
-        } else {
-            System.out.println("Cars of type " + type + ":");
-            for (Car car : cars) {
-                System.out.println(car.toString());
+        System.out.println("Browsing cars by type: " + type);
+        for (Car car : inventory.getCars()) {
+            if (car.getType().equalsIgnoreCase(type)) {
+                System.out.println(car);
             }
         }
     }
 
-    public void filterBySeats(int seats){
-        List<Car> cars = inventory.filterBySeats(seats);
-        if (cars.isEmpty()) {
-            System.out.println("No cars found.");
-        } else {
-            System.out.println("Cars with matching " + seats + " of seats:");
-            for (Car car : cars) {
-                System.out.println(car.toString());
+    public void filterBySeats(int numSeats) {
+        System.out.println("Filtering cars by number of seats: " + numSeats);
+        for (Car car : inventory.getCars()) {
+            if (car.getSeats() >= numSeats) {
+                System.out.println(car);
+            }
         }
     }
-    
-    public void bookRental(int carId, Customer customer, LocalDate startDate, LocalDate endDate) {
-        // Get the car by its ID
+
+    public void viewCarDetails(int carId) {
         Car car = inventory.getCarById(carId);
-    
-        // Create a new rental object
-        Rental rental = new Rental(car, customer, startDate, endDate);
-    
-        // Add the rental to the car's rental history
-        car.addRental(rental);
-    
-        // Print out the rental details
-        System.out.println("Rental booked successfully:");
-        System.out.println(rental.toString());
+        if (car != null) {
+            System.out.println(car);
+        } else {
+            System.out.println("Car not found");
+        }
     }
-    
-}
+
+    public void viewAvailableCars() {
+        System.out.println("Available cars:");
+        for (Car car : inventory.getAvailableCars()) {
+            System.out.println(car);
+        }
+    }
+
+    public void bookRental(int carId, Customer customer, LocalDate startDate, LocalDate endDate) {
+        Car car = inventory.getCarById(carId);
+        if (car == null) {
+            System.out.println("Car not found");
+            return;
+        }
+
+        if (!inventory.isCarAvailable(car, startDate, endDate)) {
+            System.out.println("Car is not available for rental during the specified dates");
+            return;
+        }
+
+        Rental rental = new Rental(car, customer, startDate, endDate);
+        viewRentalDetails(rental);
+    }
+
+    public void viewRentalDetails(Rental rental) {
+        System.out.println("Rental details:");
+        System.out.println("Car: " + rental.getCar().getBrand() + "
