0e39accea7f572f3d335349331bc2f4b1ee2fb50
SeanAth017 <sean@atho.com.au>
2023-03-19 18:00:28 +1100

    Comments for Main Class

--- src/rentalservice/Main.java
+++ src/rentalservice/Main.java
@@ -1,7 +1,12 @@
 package rentalservice;
+
 // Entry Point for MyCar application.
+/**
+ * The Main class is the entry point for the MyCar application.
+ */
 public class Main {
     public static void main(String[] args) {
+        // Create a new ConsoleUI object and start the application.
         ConsoleUI ui = new ConsoleUI();
         ui.start();
     }
